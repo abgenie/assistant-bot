bdf780b32115b08e988cb6e5ed0931bd7876d491
Andrei Blednov <ablednov508@gmail.com>
2023-06-06 22:56:28 +0300

    Simplify parse.py

--- parse.py
+++ parse.py
@@ -2,62 +2,27 @@ import urllib3
 import requests
 from datetime import date, timedelta
 from bs4 import BeautifulSoup
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.firefox.options import Options
-from data import *
+
 
 urllib3.disable_warnings()
-from_what_date_to_parse = date.today() - timedelta(days=how_many_days_ago_to_parse)
 
 
 def main():
 
-    # Проходим по всем разделам всех сайтов
-    write_title_to_file('Газпромбанк')
-    for url in GAZPROM_BLOGS:
-        parse_gazprom(url)
+    articles_gazprom = parse_gazprom('https://gazprombank.investments/blog/')
+    write_articles_to_file('ГАЗПРОМБАНК', articles_gazprom)
 
-    write_title_to_file('Открытие')
-    for url in OPEN_BLOGS:
-        parse_open(url)
+    articles_tinkoff = parse_tinkoff('https://www.tinkoff.ru/invest/research/all/')
+    write_articles_to_file('ТИНЬКОФФ', articles_tinkoff)
 
-    write_title_to_file('Доход')
-    for url in DOHOD:
-        parse_dohod(url)
-
-    write_title_to_file('Тинькофф')
-    parse_tinkoff(TINKOFF)
+    articles_dohod = parse_dohod('https://www.dohod.ru/analytic/research')
+    write_articles_to_file('ДОХОД', articles_dohod)
 
 
-def select_sort_new_for_open(url: str) -> BeautifulSoup:
-    """Выбирает метод сортировки статей 'Сначала новое',
-    возвращает объект 'BeautifulSoup' с отсортированными статьями"""
-    options = Options()
-    options.arguments.append('-headless')
-    driver = webdriver.Firefox(options=options)
-    driver.get(url)
-
-    # Переключаем метод сортировки записей 'Сначала новое'
-    driver.find_element(By.ID, 'multiselect').click()
-    driver.find_element(By.CLASS_NAME, 'DropdownRow_content__2VdTx').click()
-
-    soup = BeautifulSoup(driver.page_source, 'html.parser')
-
-    driver.close()
-    return soup
-
-
-def write_title_to_file(title: str) -> None:
-    """Записывает название сайта в файл"""
-    with open(filename, 'a', encoding='utf8') as f:
-        f.write(f'<strong>{title}</strong>\n')
-
-
-def write_articles_to_file(section_title: str, articles: list[str]) -> None:
+def write_articles_to_file(title: str, articles: list[str]) -> None:
     """Сохраняет полученные данные в файл"""
-    with open(filename, 'a', encoding='utf8') as f:
-        f.write(section_title + '\n')
+    with open('message.txt', 'a', encoding='utf8') as f:
+        f.write(title + '\n')
         for article in articles:
             f.write(article)
         f.write('\n')
@@ -106,108 +71,35 @@ def string_to_date(date_str: str) -> date:
     return date(int(year), month, int(day))
 
 
-def parse_gazprom(url: str) -> None:
+def parse_gazprom(url: str) -> list[str]:
     """Парсит данные с сайта 'https://gazprombank.investments'"""
 
     response = requests.get(url, headers={'User-agent': 'Mozilla/5.0'}, verify=False).text
     soup = BeautifulSoup(response, 'html.parser')
 
-    section_title = soup.h1.text.upper()
-
     articles = []
-    cards = soup.find_all('a', 'card')
-    for card in cards:
-        # Получаем дату и сравниваем с датой последнего парсинга
-        article_date = string_to_date(card.find('span', 'card-date').text)
-        if article_date < from_what_date_to_parse:
-            continue
 
-        article_link = url + card['href'].split('/')[-2]
-        article_title = card.find('span', 'card-title').text.strip()
+    section = soup.find('div', 'blog-grid')
+
+    wide_card = section.find('a', 'article-wide__link')
+    article_date = string_to_date(wide_card.find('span', 'stats__creation-date').text)
+    article_link = url + wide_card['href'][6:]
+    article_title = wide_card.find('h1').text.strip()
+
+    articles.append(f'{article_date}\n<a href="{article_link}">{article_title}</a>\n')
+
+    cards = section.find_all('a', 'article-card__link')
+    for card in cards:
+        article_date = string_to_date(card.find('span', 'stats__creation-date').text)
+        article_link = url + card['href'][6:]
+        article_title = card.find('h3').text.strip()
 
         articles.append(f'{article_date}\n<a href="{article_link}">{article_title}</a>\n')
 
-    if articles:
-        write_articles_to_file(section_title, articles)
+    return articles
 
 
-def parse_open(url: str) -> None:
-    """Парсит данные с сайта 'https://journal.open-broker.ru'"""
-
-    response = requests.get(url, headers={'User-agent': 'Mozilla/5.0'}, verify=False).text
-    soup = BeautifulSoup(response, 'html.parser')
-
-    section_title = soup.h1.text.upper()
-
-    # Если сортировка не по дате, то переключаем её с помощью Selenium и возвращаем новый soup
-    if not soup.find('span', 'MultiSelect_multiselectText__38MZ5').text == 'Сначала новое':
-        soup = select_sort_new_for_open(url)
-
-    articles = []
-    cards = soup.find_all('a', 'CardSmall_card__1SmUx Category_rootArticle__1XgQa')
-    for card in cards:
-        # Получаем дату и проверяем, сравниваем с датой последнего парсинга
-        raw_date = card.find('p', 'CardSmall_noWrap__3I1yt Paragraph-module__paragraph--g4xw7 '
-                                  'Paragraph-module__paragraph--p4--uYcdM '
-                                  'Paragraph-module__paragraph--fixed_60--Mqv6o').text[:-8]
-
-        try:
-            # Проверяем указан ли в конце год
-            int(raw_date[-4:])
-        except ValueError:
-            # Если не указан, добавляем текущий год
-            raw_date += ' ' + str(date.today().year)
-
-        article_date = string_to_date(raw_date)
-        if article_date < from_what_date_to_parse:
-            break
-
-        article_link = url + card['href'].split('/')[-2]
-        article_title = card.find('p', 'CardSmall_cardTitle__bv6e3 Paragraph-module__paragraph--g4xw7 '
-                                       'Paragraph-module__paragraph--p1--iBqRO').text
-        article_title = article_title.replace(' ', ' ').strip()
-
-        articles.append(f'{article_date}\n<a href="{article_link}">{article_title}</a>\n')
-
-    if articles:
-        write_articles_to_file(section_title, articles)
-
-
-def parse_dohod(url: str) -> None:
-    """Парсит данные с сайта 'https://www.dohod.ru'"""
-
-    response = requests.get(url, headers={'User-agent': 'Mozilla/5.0'}, verify=False).text
-    soup = BeautifulSoup(response, 'html.parser')
-
-    section_title = soup.h1.text.upper()
-
-    articles = []
-    if url == 'https://www.dohod.ru/analytic/research':
-        cards = soup.find_all('a', 'clr-black t-tdn products__slide slider-main__slide '
-                                   'swiper-slide products__slide--chart bg-white products__analytic_blog_item')
-    else:
-        cards = soup.find_all('a', 't-tdn article__link')
-
-    for card in cards:
-        if url == 'https://www.dohod.ru/analytic/research':
-            article_date = string_to_date(card.time.text)
-        else:
-            list_date = [int(x) for x in card.time['datetime'].split('-')]
-            article_date = date(*list_date)
-
-        if article_date < from_what_date_to_parse:
-            break
-
-        article_link = 'https://www.dohod.ru/' + card['href']
-        article_title = card.h4.text.strip()
-
-        articles.append(f'{article_date}\n<a href="{article_link}">{article_title}</a>\n')
-
-    if articles:
-        write_articles_to_file(section_title, articles)
-
-
-def parse_tinkoff(url: str) -> None:
+def parse_tinkoff(url: str) -> list[str]:
     """Парсит данные с сайта 'https://www.tinkoff.ru'"""
 
     response = requests.get(url, headers={'User-agent': 'Mozilla/5.0'}, verify=False)
@@ -215,20 +107,36 @@ def parse_tinkoff(url: str) -> None:
     soup = BeautifulSoup(response.text, 'html.parser')
 
     articles = []
-    cards = soup.find_all('a', 'Link-module__link_UWSCx Link-module__link_theme_default_s_i0Q')
+    cards = soup.find_all('a', 'Link-module__link_UWSCx Link-module__link_theme_default_s_i0Q')[:6]
     for card in cards:
-        # Получаем дату и проверяем, сравниваем с датой последнего парсинга
         article_date = string_to_date(card.find('div', 'ResearchCatalogNews__date_Irh8b').text)
-        if article_date < from_what_date_to_parse:
-            break
-
         article_link = card['href']
         article_title = card.find('div', 'ResearchCatalogNews__title_c1Sx2').text.strip()
 
         articles.append(f'{article_date}\n<a href="{article_link}">{article_title}</a>\n')
 
-    if articles:
-        write_articles_to_file('Все статьи', articles)
+    return articles
+
+
+def parse_dohod(url: str) -> list[str]:
+    """Парсит данные с сайта 'https://www.dohod.ru'"""
+
+    response = requests.get(url, headers={'User-agent': 'Mozilla/5.0'}, verify=False).text
+    soup = BeautifulSoup(response, 'html.parser')
+
+    articles = []
+
+    cards = soup.find_all('a', 'clr-black t-tdn products__slide slider-main__slide '
+                               'swiper-slide products__slide--chart bg-white products__analytic_blog_item')
+
+    for card in cards:
+        article_date = string_to_date(card.time.text)
+        article_link = 'https://www.dohod.ru/' + card['href']
+        article_title = card.h4.text.strip()
+
+        articles.append(f'{article_date}\n<a href="{article_link}">{article_title}</a>\n')
+
+    return articles
 
 
 if __name__ == '__main__':
